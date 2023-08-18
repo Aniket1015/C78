151e6a5b0bd4999b94022b568936535d4a19019f
Aniket1015 <136613382+Aniket1015@users.noreply.github.com>
2023-08-18 16:48:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+var cars=["BMW","Audi","Ferrari"];
+console.log(cars);
+
+cars.push("hyundai");
+console.log(cars);
+
+cars.sort();
+console.log(cars);
+
+cars.unshift("Maruti Suzuki");
+console.log(cars);
+
+var a =cars.length;
+console.log(a);
+
+cars.shift();
+console.log(cars);
+
+cars.pop();
+console.log(cars);
+
+var flowers=["rose","lavender","lotus"];
+console.log(flowers);
+
+flowers.sort();
+console.log(flowers);
+
+flowers.reverse();
+console.log(flowers);
+
